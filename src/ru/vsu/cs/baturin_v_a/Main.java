5829124f0e33bf4206d935c7dfd595e45804c74e
CooIDragon <90614965+CooIDragon@users.noreply.github.com>
2021-10-27 15:04:22 +0300

    Update Main.java

--- src/ru/vsu/cs/baturin_v_a/Main.java
+++ src/ru/vsu/cs/baturin_v_a/Main.java
@@ -1,6 +1,8 @@
 package ru.vsu.cs.baturin_v_a;
+
 import java.util.Locale;
 import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -12,40 +14,40 @@ public class Main {
         System.out.print("Введите ширину = ");
         int w = readVar();
 
-        if (testVar(h) && testVar(w)){
+        if (testVar(h) && testVar(w)) {
             printLines(h, w);
         }
     }
 
-    private static boolean testVar(int k){
+    private static boolean testVar(int k) {
         boolean m = k >= 3;
         return m;
     }
 
-    private static int readVar(){
+    private static int readVar() {
         Scanner sc = new Scanner(System.in);
         int k = sc.nextInt();
 
         return k;
     }
 
-    private static void printLine(int h, int w){
+    private static void printLine(int h, int w) {
         System.out.print("*");
 
-        for (int i = 0; i < (w - 2); i++){
+        for (int i = 0; i < (w - 2); i++) {
             System.out.print("-");
         }
 
         System.out.println("*");
     }
 
-    private static void printLines(int h, int w){
+    private static void printLines(int h, int w) {
         printLine(h, w);
 
-        for (int i = 0; i < (h - 2); i++){
+        for (int i = 0; i < (h - 2); i++) {
             System.out.print("|");
 
-            for (int j = 0; j < (w - 2); j++){
+            for (int j = 0; j < (w - 2); j++) {
                 System.out.print(" ");
             }
 
